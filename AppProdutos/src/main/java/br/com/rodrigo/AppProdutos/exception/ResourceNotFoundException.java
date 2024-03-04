5fcef7be0da6915cb49cb500a83e0cda72288af3
asprodrigosouza <tenrodrigo@yahoo.com.br>
2024-03-04 09:13:21 -0300

    JWT Token - Correção Exception personalizada

--- AppProdutos/src/main/java/br/com/rodrigo/AppProdutos/exception/ResourceNotFoundException.java
+++ AppProdutos/src/main/java/br/com/rodrigo/AppProdutos/exception/ResourceNotFoundException.java
@@ -1,5 +1,7 @@
 package br.com.rodrigo.AppProdutos.exception;
 
+import org.springframework.http.HttpStatus;
+
 public class ResourceNotFoundException extends RuntimeException {
 	
 	public ResourceNotFoundException(String message) {
